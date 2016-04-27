5a15d0497e4536bb505aa78ae7da55c326fe77da
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2016-04-27 18:13:41 +0200

    ajout des classes Promotion et Produit

---
+++ Model/Produit.php
@@ -0,0 +1,14 @@
+<?php
+Class Produit{
+	//Attributs de Produit
+	
+	private $reference;
+	private $libelle;
+	private $marque;
+	private fichier_image;
+	private prix_unit_HT;
+	private liquide_VF;
+	private prix_kilo_ou_litre;
+	private quandtite_stock;
+}
+?>
---
+++ Model/Promotion.php
@@ -0,0 +1,23 @@
+<?php
+class Promotion{
+	//Attributs de promotion
+	
+	private $code_promo;
+	private $date_debut;
+	private $date_fin;
+	private $max_par_client;
+	
+}
+
+class P_individuelle extends Promotion{
+	
+	private $reduction_absolue;
+	private $reduction_relative;
+	private immediate_VF;
+}
+
+class P_lot extends Promotion{
+	private nb_achetes;
+	private nb_gratuits;
+}
+?>
