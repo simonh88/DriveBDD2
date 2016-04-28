2576681128a191cfac79f2d0e358f8705a083240
Krack54 <teragon54@hotmail.fr>
2016-04-28 16:40:24 +0200

    getAll

--- Model/SSR_P.php
+++ Model/SSR_P.php
@@ -28,5 +28,29 @@ class SRR_P{
         $this->nom_ssr = $nom_ssr;
     }
 
+    
+    public static function getAll() { // exemple, a suprimer
+        $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
 
+        $stid = oci_parse($oci, 'SELECT * FROM SSR_P.php'); // prepare le code
+        $r = oci_execute($stid); // on l'execute
+        if (!$r) {
+            $e = oci_error($stid);
+            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+        }
+
+        $data = array();
+        
+        $i = 0;
+        while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+            $client = new Client($row);
+            $data[$i] = $client;
+            $i++;
+        }
+        
+        return $data;
+    }
+
+    
+    
 }
