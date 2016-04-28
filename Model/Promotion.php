9255174e8fe2fb3e60d8bbb1858a92736f3b083a
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2016-04-28 15:53:54 +0200

    test

--- Model/Promotion.php
+++ Model/Promotion.php
@@ -1,54 +1,52 @@
 <?php
 class Promotion{
-	//Attributs de promotion
-	
-	private $code_promo;
-	private $date_debut;
-	private $date_fin;
-	private $max_par_client;
-        
-        function __construct($row) {
-            $this->code_promo = $row['CODE_PROMO'];
-            $this->date_debut = $row['DATE_DEBUT'];
-            $this->date_fin = $row['DATE_FIN'];
-            $this->max_par_client = $row['MAX_PAR_CLIENT'];
-        }
+    //Attributs de promotion
 
-        
-        //GETTER
-        function getCode_promo() {
-            return $this->code_promo;
-        }
+    private $code_promo;
+    private $date_debut;
+    private $date_fin;
+    private $max_par_client;
 
-        function getDate_debut() {
-            return $this->date_debut;
-        }
+    function __construct($row) {
+        $this->code_promo = $row['CODE_PROMO'];
+        $this->date_debut = $row['DATE_DEBUT'];
+        $this->date_fin = $row['DATE_FIN'];
+        $this->max_par_client = $row['MAX_PAR_CLIENT'];
+    }
 
-        function getDate_fin() {
-            return $this->date_fin;
-        }
+    //GETTER
+    function getCode_promo() {
+        return $this->code_promo;
+    }
 
-        function getMax_par_client() {
-            return $this->max_par_client;
-        }
+    function getDate_debut() {
+        return $this->date_debut;
+    }
 
-        //SETTER
-        function setCode_promo($code_promo) {
-            $this->code_promo = $code_promo;
-        }
+    function getDate_fin() {
+        return $this->date_fin;
+    }
 
-        function setDate_debut($date_debut) {
-            $this->date_debut = $date_debut;
-        }
+    function getMax_par_client() {
+        return $this->max_par_client;
+    }
 
-        function setDate_fin($date_fin) {
-            $this->date_fin = $date_fin;
-        }
+    //SETTER
+    function setCode_promo($code_promo) {
+        $this->code_promo = $code_promo;
+    }
 
-        function setMax_par_client($max_par_client) {
-            $this->max_par_client = $max_par_client;
-        }
+    function setDate_debut($date_debut) {
+        $this->date_debut = $date_debut;
+    }
 
+    function setDate_fin($date_fin) {
+        $this->date_fin = $date_fin;
+    }
+
+    function setMax_par_client($max_par_client) {
+        $this->max_par_client = $max_par_client;
+    }
 
 }
 ?>
