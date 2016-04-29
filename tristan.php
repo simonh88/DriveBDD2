2aafe70864d9aab772013721814d09962c6ee36f
Krack54 <teragon54@hotmail.fr>
2016-04-29 14:23:15 +0200

    Promotion

--- tristan.php
+++ tristan.php
@@ -3,9 +3,4 @@ require_once('common.php');
 
 
 $cat = Rayon::getSesSRayon('Eaux');
-//$cat = Rayon::getAll();
-//var_dump($cat); 
-
-
-$prod = Categorie::getAllProduit('Boissons');
-var_dump($prod);
+var_dump($cat); 
