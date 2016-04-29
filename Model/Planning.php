ce64e7920d8312ba7f1ca8ae95a716ba13f66740
Krack54 <teragon54@hotmail.fr>
2016-04-29 10:34:30 +0200

    getAll

--- Model/Planning.php
+++ Model/Planning.php
@@ -1,16 +1,16 @@
 <?php
 
-class Planning{
+class Planning {
+
     //Attributs
     private $date_heure;
     private $nombre_livraison_max;
-    
+
     function __construct($row) {
         $this->date_heure = $row['DATE_HEURE'];
         $this->nombre_livraison_max = $row['NOMBRE_LIVRAISON_MAX'];
     }
 
-    
     //GETTER
     function getDate_heure() {
         return $this->date_heure;
@@ -29,8 +29,7 @@ class Planning{
         $this->nombre_livraison_max = $nombre_livraison_max;
     }
 
-    
-    public static function getAll() { // exemple, a suprimer
+    public static function getAll() {
         $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
 
         $stid = oci_parse($oci, 'SELECT * FROM Planning'); // prepare le code
@@ -41,16 +40,25 @@ class Planning{
         }
 
         $data = array();
-        
+
         $i = 0;
         while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
             $client = new Planning($row);
             $data[$i] = $client;
             $i++;
         }
-        
+
         return $data;
     }
-    
-}
 
+    public static function getInfosPlanning($date) {
+        $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
+
+        $stid = oci_parse($oci, 'SELECT * FROM Planning where date_heure = :id'); // prepare le code
+
+        oci_bind_by_name($stid, ':id', $id_carte);
+
+        $r = oci_execute($stid); // on l'execute
+    }
+
+}
