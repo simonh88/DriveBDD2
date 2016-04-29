b0c4f43af2bcec007f49c0a1b2393fbe2b68be95
Krack54 <teragon54@hotmail.fr>
2016-04-29 15:28:38 +0200

    TODO

--- index.php
+++ index.php
@@ -7,5 +7,41 @@
  */
 
 header('location:drive.php?a=Accueil');
-
+    /*
+     * TODO 
+     * 
+     * INSERT PANIER lorsque le client n'en a pas (check lors de la 
+     * connexion du client)
+     *
+     * 
+     * INSERT PLanning DateDefautPlanning (avec limit -1)
+     * 
+     * INSERT INTO PANIER(nocarte, VIDE_VF, DatePlanningDefault,0,0)
+     * 
+     * DatePDefault = date du lendemain
+     * Pourra etre changer lors du paiement
+     *
+     * FLUSH PANIER && ITEM lors du payement payement (ou boutton)
+     * 
+     * UPDATE dateValidation et de montant (lors du Payement)
+     * 
+     * 
+     *Rajouter un fonction dans Planning ou Panier qui verif si 
+     *
+     * select count(*) from Panier where Date_Heure = DateDefaultHeure <
+     * select nbmax from Planning
+     * 
+     * si Planning n'existait pas * INSERT PLANNING dateCHoisi
+     * 
+     * pour lors du choix de la date de livraison
+     * 
+     * INSERT PLanning
+     * INSERT PANIER
+     * DELETE ON CASCADE PANIER
+     * DELETE ITEM
+     * UPDATE ITEM & PANIER
+     * fonction getNBPanier -> verifNombreLivraison
+     */
 ?>
+
+
