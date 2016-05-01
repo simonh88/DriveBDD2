538e388a27e2529c03e27e5147d997778031137b
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2016-05-01 22:30:35 +0200

    On peut maintenant ajouter/supprimer dans un panier et le vider

--- Model/Item.php
+++ Model/Item.php
@@ -1,17 +1,17 @@
 <?php
 
-class Item{
+class Item {
+
     private $no_carte;
     private $reference;
     private $quantite;
-    
+
     function __construct($row) {
         $this->no_carte = $row['NO_CARTE'];
         $this->reference = $row['REFERENCE'];
         $this->quantite = $row['QUANTITE'];
     }
 
-    
     //GETTER
     function getNo_carte() {
         return $this->no_carte;
@@ -38,8 +38,6 @@ class Item{
         $this->quantite = $quantite;
     }
 
-    
-    
     public static function getAll() { // exemple, a suprimer
         $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
 
@@ -51,24 +49,26 @@ class Item{
         }
 
         $data = array();
-        
+
         $i = 0;
         while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
             $client = new Item($row);
             $data[$i] = $client;
             $i++;
         }
-        
+
         return $data;
     }
-    /**à partir du no_carte on recup le contenu du panier**/
-    public static function getInfosPanier($id_carte){
+
+    /*     * à partir du no_carte on recup le contenu du panier* */
+
+    public static function getInfosPanier($id_carte) {
         $oci = Base::getConnexion();
         $stid = oci_parse($oci, 'SELECT *
                 FROM Item WHERE no_carte = :id'); // prepare le cod 
-  
+
         oci_bind_by_name($stid, ':id', $id_carte);
-        
+
         $r = oci_execute($stid); // on l'execute
         if (!$r) {
             $e = oci_error($stid);
@@ -83,5 +83,71 @@ class Item{
         }
         return $data;
     }
-}
 
+    /** fonction testant la présence d'un produit ou non dans un panier * */
+    public static function existProduitDansPanier($noCarte, $ref) {
+        $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
+        $stid = oci_parse($oci, "SELECT count(*) as nb FROM Item WHERE reference = :id and no_carte = :no ");
+        oci_bind_by_name($stid, ':id', $ref);
+        oci_bind_by_name($stid, ':no', $noCarte);
+        $r = oci_execute($stid); // on l'execute
+        if (!$r) {
+            $e = oci_error($stid);
+            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+        }
+
+        $row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
+        if ($row['NB'] == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Inserer un produit avec sa reference et sa quantite* */
+    public static function insertUnProduit($noCarte, $ref, $quant) {
+        //S'il existe pas on peut l'ajouter
+        if (!Item::existProduitDansPanier($noCarte, $ref)) {
+            $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
+            $stid = oci_parse($oci, "INSERT INTO Item VALUES (:carte,:ref,:quant)");
+            oci_bind_by_name($stid, ':carte', $noCarte);
+            oci_bind_by_name($stid, ':ref', $ref);
+            oci_bind_by_name($stid, ':quant', $quant);
+            $r = oci_execute($stid); // on l'execute
+            if (!$r) {
+                $e = oci_error($stid);
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            }
+        }
+        //Sinon on fait rien.
+    }
+
+    /**Supprime un seul produit**/
+    public static function deleteUnProduit($noCarte, $ref) {
+        //S'il existe pas on peut l'ajouter
+        if (!Item::existProduitDansPanier($noCarte, $ref)) {
+            $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
+            $stid = oci_parse($oci, "DELETE FROM Item WHERE no_carte = :carte and reference = :ref");
+            oci_bind_by_name($stid, ':carte', $noCarte);
+            oci_bind_by_name($stid, ':ref', $ref);
+            $r = oci_execute($stid); // on l'execute
+            if (!$r) {
+                $e = oci_error($stid);
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            }
+        }
+        //Sinon on fait rien.
+    }
+    
+    /**Supprime tout le contenu du panier.**/
+    public static function deleteAll($noCarte){
+            $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
+            $stid = oci_parse($oci, "DELETE FROM Item WHERE no_carte = :carte");
+            oci_bind_by_name($stid, ':carte', $noCarte);
+            $r = oci_execute($stid); // on l'execute
+            if (!$r) {
+                $e = oci_error($stid);
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            }
+    }
+
+}
