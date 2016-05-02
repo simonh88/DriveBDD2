9788584011365fd304df25b9271758bd660d7c7a
krack54 <teragon54@hotmail.fr>
2016-05-02 17:34:31 +0200

    Css connexion

--- Vue/ConnexionVue.php
+++ Vue/ConnexionVue.php
@@ -4,16 +4,16 @@ class ConnexionVue extends MainVue {
 
     public function displayBody() {
         ?>  <div  style="
-    display: inline-block;
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 200px;
-    height: 100px;
-    margin: auto;
-    background-color: #f3f3f3; >
+              display: inline-block;
+              position: fixed;
+              top: 0;
+              bottom: 0;
+              left: 0;
+              right: 0;
+              width: 200px;
+              height: 100px;
+              margin: auto;
+              background-color: #f3f3f3;">
             <form class="form-inline">
                 <div class="form-group">
                     <input class="form-control form-control-lg" placeholder="numero carte fidelite">
