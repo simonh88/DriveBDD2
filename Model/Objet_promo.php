c7fbe8d4415016fbe990521c3c52a9bf401479e3
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2016-05-09 15:56:28 +0200

    tristan ne teste rien du tout batard

--- Model/Objet_promo.php
+++ Model/Objet_promo.php
@@ -98,7 +98,7 @@ class Objet_promo {
 
     public static function insert($code_promo, $produit) {
 
-        if (!empty(Objet_promo::getCodeFcode($produit))) {
+        if (!empty(Objet_promo::getCodePromoFcode($produit))) {
             Objet_promo::delete($code_promo, $produit);
         }
         $oci = Base::getConnexion(); // on recupere la connexion a la base de donnée
