6ce6bc2ecfc43d2dca3eaf9b146cf1dcfc589010
krack54 <teragon54@hotmail.fr>
2016-05-14 16:11:51 +0200

    Datepicker

--- Vue/PayerVue.php
+++ Vue/PayerVue.php
@@ -58,9 +58,19 @@ class PayerVue extends MainVue {
                             </div>
                             <script type="text/javascript">
                                 $(function () {
+
                                     $('#datetimepicker12').datetimepicker({
                                         inline: true,
-                                        sideBySide: true
+                                        sideBySide: true,
+                                        minDate: 0,
+                                        onGenerate: function (ct) {
+                                            jQuery(this).find('.xdsoft_date.xdsoft_weekend')
+                                                    .addClass('xdsoft_disabled');
+                                        },
+                                        weekends: ['01.01.2014', '02.01.2014', '03.01.2014', '04.01.2014', '05.01.2014', '06.01.2014'],
+                                        allowTimes: [
+                                            '8:00','8:30','9:00','9:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30'
+                                        ]
                                     });
                                 });
                             </script>
@@ -71,8 +81,10 @@ class PayerVue extends MainVue {
                             <input type="hidden" name="eurosADeduire" value="<?php echo($this->eurosAEnlever) ?>">
                             <button type="submit" class="btn btn-success" form="isPayer">Finaliser</button>
                         </form>
-                    <?php }
-                } ?>
+                        <?php
+                    }
+                }
+                ?>
             </div>
         </body>
         <?php
